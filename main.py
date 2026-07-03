5cf10da8e977256506e17ff4ab1393c68cb98c1e
Tomáš Janečka <tomjanecka11@gmail.com>
2026-07-03 10:32:44 +0200

    Fix root script

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "Api.Api:app", 
-        host="127.0.0.1", 
-        port=8000, 
-        reload=True
-    )
+from Api.Api import app
